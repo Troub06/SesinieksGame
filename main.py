95505b5faf3f7f78aa1069638e3aee3fbb48fae2
Egor <yegor.skuratov@gmail.com>
2025-04-03 12:11:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import pygame
+from src.game import Game
 
+def main():
+    pass
+
+if __name__ == "__main__":
+    game = Game()
+    game.main()
+
+# class Game():
+#     def __init__(self, width: int, height: int):
+#         pygame.init()
+#         self.screen = pygame.display.set_mode((width, height))
+#         self.clock = pygame.time.Clock()
+#         self.running = True
+
+# game = Game(1280, 720)
+# while game.running:
+#     # poll for events
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             game.running = False
+
+#     # fill the screen with a color to wipe away anything from last frame
+#     game.screen.fill("lightgreen")
+
+#     # RENDER YOUR GAME HERE
+
+#     # flip() the display to put your work on screen
+#     pygame.display.flip()
+
+#     game.clock.tick(60)  # limits FPS to 60
+
+# pygame.quit()
