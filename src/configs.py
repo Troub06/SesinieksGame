7846ee3e24b5b6531593d1700185bfb8ac9922ad
Egor <yegor.skuratov@gmail.com>
2025-04-04 10:41:35 +0300

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -1,4 +1,8 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 820
+GRID_IMAGE_POSITION = (265, 210)
+PLAYER_GRID_IMAGE_POSITION = (755, 30)
 
-loading_image = "assets/MrBigPig.jpg"
+loading_image = "assets\\bg.png"
+grid_image = "assets\\grid.png"
+player_grid_image = "assets\\player_grid.png"
